1f5b1bd5e64b192d742608088d6062bf45118011
Justin Stephenson <jstephenson@protonmail.com>
2021-09-03 15:54:16 -0500

    wire up crud actions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useReducer, useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
@@ -15,7 +15,8 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { conditionalExpression, STATEMENT_OR_BLOCK_KEYS } from '@babel/types';
+import { modes } from 'react-transition-group/SwitchTransition';
+import { moduleExpression } from '@babel/types';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const App = () => {
+
   const [open, setOpen] = React.useState(false);
   const [rows, setRows] = React.useState([
     { id: 1, address: 'Snow', name: 'Jon' },
@@ -42,31 +44,79 @@ const App = () => {
     { id: 8, address: 'Frances', name: 'Rossini' },
     { id: 9, address: 'Roxie', name: 'Harvey' },
   ]);
+  const initialFormValues = {
+    name: "",
+    id: 99999,
+    address: "",
+    formSubmitted: false,
+    success: false,
+  };
+  const [values, setValues] = React.useState(initialFormValues);
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [formState, setFormState] = useState({
+    isEditable: false,
+    dialogTitle: "",
+    submitText: "",
+    rowId: 0,
+  });
   const classes = useStyles();
 
-  const handleOpen = (e, cellValues) => {
-    setOpen(true);
-  };
-  const handleEdit = (e, cellValues) => {
-    alert('TODO: ACTUALLY SET STATE; (include button text "save")');
-    setOpen(true);
-    console.log(e, cellValues.row.name, cellValues.row.address)
-    // STOPPING POINT
-  };
-  const handleCreate = () => {
-    alert('USE ACTUAL FORM INPUT, NOT HARDCODED');
-    setRows(rows => [...rows, { id: 10, address: 'Foo', name: 'Bar' }])
-    setOpen(false);
-  };
-  const handleDelete = () => {
-    alert('TODO');
-    setOpen(false);
-  };
   const handleClose = () => {
     setOpen(false);
   };
-  const handleSubmit = () => {
-    alert('here')
+  const handleCreate = (e, cellValues) => { // TODO RENAME
+    setOpen(true);
+    setFormState({
+      isEditable: false,
+      dialogTitle: "Add New Hospital",
+      submitText: "Create",
+      rowId: 0,
+    })
+  };
+  const handleDelete = ({id}) => {
+    const deletionIndex = rows.map(row => row.id).indexOf(id);
+    const deletedRows = [...rows.filter((row) => row.id === id)];
+    setRows(
+      rows.filter((row) => deletedRows.filter((deletion) => deletion.id === row.id).length < 1)
+    );
+    setOpen(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    values.id = values.id + 1;
+    console.log("values.id", values.id)
+    if (formState.isEditable) {
+      console.log("EDITING")
+      const updatedRows = [...rows];
+      const updatedRow = { id: formState.rowId };
+      updatedRow.name = name ? name : updatedRows[formState.updateIndex].name;
+      updatedRow.address = address ? address : updatedRows[formState.updateIndex].address;
+      updatedRows[formState.updateIndex] = updatedRow;
+      setRows(updatedRows);
+    } else {
+      console.log("CREATING")
+      setRows(rows => [...rows, {
+        id: values.id,
+        name,
+        address,
+      }])
+    }
+    setOpen(false);
+  };
+  const handleUpdate = (e, { row }) => {
+    setOpen(true);
+    const updateIndex = rows.map(r => r.id).indexOf(row.id);
+    setName(row.name)
+    setAddress(row.address)
+    setFormState({
+      isEditable: true,
+      dialogTitle: `Update ${row.name}`,
+      submitText: `Update`,
+      rowId: row.id,
+      updateIndex,
+    });
   };
 
   const columns: GridColDef[] = [
@@ -96,8 +146,8 @@ const App = () => {
       renderCell: (cellValues) => {
         return (
           <>
-            <Button color="primary" onClick={(event) => {handleEdit(event, cellValues)}}>Edit</Button>
-            <Button color="primary" onClick={(event) => {handleDelete(event, cellValues)}}>Delete</Button>
+            <Button color="primary" onClick={(event) => {handleUpdate(event, cellValues)}}>Edit</Button>
+            <Button color="primary" onClick={(event) => {handleDelete(cellValues)}}>Delete</Button>
           </>
        );
       }
@@ -113,58 +163,40 @@ const App = () => {
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Manage Hospitals
-            </Typography>
-            <Button color="inherit" onClick={handleOpen}>Add</Button>
+            <Typography variant="h6" className={classes.title}>Manage Hospitals</Typography>
+            <Button color="inherit" onClick={handleCreate}>Add</Button>
           </Toolbar>
         </AppBar>
         <div style={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            disableSelectionOnClick
-          />
+          <DataGrid rows={rows} columns={columns} pageSize={5} disableSelectionOnClick />
         </div>
       </Container>
-
       <div>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <form onSubmit={handleSubmit}>
-        <DialogTitle id="form-dialog-title">Add New Hospital</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            (You deserve a better address input, I know.)
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="address"
-            label="Address"
-            type="text"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button type="button" onClick={handleClose} color="secondary">
-            Cancel
-          </Button>
-          <Button typre="submit" onClick={handleCreate} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </form>
-    </Dialog>
-  </div>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <form onSubmit={handleSubmit}>
+            <DialogTitle id="form-dialog-title">{formState.dialogTitle}</DialogTitle>
+            <DialogContent>
+              <TextField id="name" type="text" name="name" label="Name"
+                inputProps={formState.isEditable ? { defaultValue: name } : ""}
+                margin="dense" fullWidth
+                onBlur={e => setName(e.target.value)}
+                onChange={e => setName(e.target.value)}
+              />
+              <TextField
+                id="address" type="text" name="Address" label="Address"
+                inputProps={formState.isEditable ? { defaultValue: address } : ""}
+                margin="dense" fullWidth
+                onBlur={e => setAddress(e.target.value)}
+                onChange={e => setAddress(e.target.value)}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button type="button" onClick={handleClose} color="secondary">Cancel</Button>
+              <Button type="submit" color="primary">{formState.submitText}</Button>
+            </DialogActions>
+          </form>
+        </Dialog>
+      </div>
     </React.Fragment>
   );
 }
